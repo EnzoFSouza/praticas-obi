e8216cee2b372c37dbdefafcc80295c1c9706daa
Enzo Souza <80353348+EnzoFSouza@users.noreply.github.com>
2025-05-09 19:09:49 -0300

    Idade NJ 2019 F1

--- idade.py
+++ idade.py
@@ -1,12 +1,8 @@
-lista_idades = list()    
-for i in range(3):
-    lista_idades.append(int(input()))
+M = int(input())
+A = int(input())
+B = int(input())
+C = M - A - B
 
-maior_idade = max(lista_idades)
-menor_idade = min(lista_idades)
-
-#.remove() removes first occurrence of value
-lista_idades.remove(maior_idade)
-lista_idades.remove(menor_idade)
-
-print(lista_idades[0])
+lista = [A, B, C]
+lista.sort()
+print(lista[2])
